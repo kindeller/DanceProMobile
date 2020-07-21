f2cbacd8d6c3d4244ffd4b95d94888aeb714195a
kindeller <kindeller@hotmail.co.uk>
2020-07-21 21:17:23 +0800

    1.2.2 iOS
    Features:
    - Updated UI and graphics for Dark Mode
    - Added new graphics for audio and folder icons.
    - Automated Transfer Media screens connection to the DancePro network.
    - Finally added the ability to auto forget the network on app close and when disconnecting from the transfer screen.
    
    Bug and Crash Fixes:
    - Fixed network connectivity issues.
    - Fixed an issue with corrupt files crashing the app on media gallery opening.

---
+++ DancePro/Services/ErrorLogService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DancePro.Services
+{
+    /// <summary>
+    /// This service is called when an exception is thrown and caught by the application.
+    /// It allows the logging and error recalling of issues from an error log.
+    /// </summary>
+    public class ErrorService
+    {
+        private string FilePath;
+        private const string ErrorLogFileName = "ErrorLog.txt";
+        List<Error> Errors = new List<Error>();
+
+        public List<Error> GetErrors()
+        {
+
+            foreach(Error e in Errors)
+            {
+                Console.WriteLine(e);
+            }
+            return Errors;
+        }
+
+        public ErrorService(string _ErrorLogDir)
+        {
+            FilePath = _ErrorLogDir + ErrorLogFileName;
+            Init();
+        }
+
+        //Setup of service
+        public void Init()
+        {
+
+            try
+            {
+
+                if (System.IO.File.Exists(FilePath))
+                {
+                    string jsonData = System.IO.File.ReadAllText(FilePath);
+                    Errors = JsonConvert.DeserializeObject<List<Error>>(jsonData);
+                }
+
+            }
+            catch(Exception e)
+            {
+                LogError(e);
+            }
+
+
+        }
+
+
+        public void LogError(Exception e)
+        {
+            Error er = new Error();
+            er.Date = DateTime.Now;
+            er.Type = Error.ExceptionTypes.Exception;
+            er.Message = e.Message;
+            er.Source = e.Source;
+
+            Errors.Add(er);
+
+            System.IO.File.WriteAllText(FilePath, JsonConvert.SerializeObject(Errors));
+        }
+
+        public void LogError(string message)
+        {
+            Error er = new Error();
+            er.Date = DateTime.Now;
+            er.Type = Error.ExceptionTypes.Error;
+            er.Message = message;
+            er.Source = "Null";
+
+            Errors.Add(er);
+
+            System.IO.File.WriteAllText(FilePath, JsonConvert.SerializeObject(Errors));
+        }
+    }
+
+
+    public class Error
+    {
+
+        public enum ExceptionTypes
+        {
+            Exception,
+            Error,
+            Warning,
+            Log
+        }
+
+
+        public DateTime Date;
+        public ExceptionTypes Type;
+        public string Message;
+        public string Source;
+
+
+        public override string ToString()
+        {
+            return "[" + Date.ToShortDateString() + "] - " + Message;
+        }
+
+    }
+}
